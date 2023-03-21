b384ec6f01cb20d4ea99c14cb0465325e33c4418
Milosz B <miloszbargiel@gmail.com>
2023-03-21 15:22:46 +0100

    Add cw1e

---
+++ cw1/cw1e.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "display_data.h"
+
+int main()
+{
+  int status;
+  printf("Proces macierzysty:\n");
+  display_process_data();
+  printf("Procesy potomne:\n");
+  for (int i = 0; i < 3; i++)
+  {
+    int pid = fork();
+    if (pid == -1)
+    {
+      perror("fork error");
+      exit(1);
+    }
+    if (pid == 0)
+    {
+      setpgid(0, 0);
+      display_process_data();
+    }
+    if (pid > 0)
+    {
+      wait(&status);
+    }
+  }
+}
