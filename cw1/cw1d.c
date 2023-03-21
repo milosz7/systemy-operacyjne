34b59737c1554f8547cdf9784131efc80925f4e2
Milosz B <miloszbargiel@gmail.com>
2023-03-21 15:22:37 +0100

    Add cw1d

---
+++ cw1/cw1d.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include "display_data.h"
+
+#define SLEEP_TIME_IN_SECONDS 2
+#define SLEEP_TIME_PARENT_PROCESS 10
+
+int main()
+{
+  printf("Proces macierzysty:\n");
+  display_process_data();
+  printf("Procesy potomne:\n");
+  for (int i = 0; i < 3; i++)
+  {
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+      perror("fork error");
+      exit(1);
+    }
+    if (pid == 0)
+    {
+      sleep(SLEEP_TIME_IN_SECONDS + i);
+      display_process_data();
+    }
+  }
+  sleep(SLEEP_TIME_PARENT_PROCESS);
+  return 0;
+}
