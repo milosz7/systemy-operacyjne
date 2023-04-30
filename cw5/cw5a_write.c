dcc501f736d4fec6042ad4befa6b04011c406640
Milosz B <miloszbargiel@gmail.com>
2023-04-30 21:47:32 +0200

    Add code for cw5a_write helper program

---
+++ cw5/cw5a_write.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define REQUIRED_ARGS_AMOUNT 3
+#define WRITE_CHUNK 7
+#define OPEN_MODE 0644
+#define MAX_SLEEP_IN_SECS 3
+
+int random_sleep()
+{
+  srand(time(NULL));
+  return (int)(((double)rand() / (double)RAND_MAX) * MAX_SLEEP_IN_SECS) + 1;
+}
+
+int main(int argc, char *argv[])
+{ 
+  if (argc < REQUIRED_ARGS_AMOUNT)
+  {
+    fprintf(stderr, "Wrong number of arguments provided!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char buffer[WRITE_CHUNK + 1];
+  char *output_filename = argv[1];
+  char *pipe_path = argv[2];
+  ssize_t bytes_read;
+
+  ssize_t pipedes_read = open(pipe_path, O_RDONLY, OPEN_MODE);
+  ssize_t output_descriptor = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, OPEN_MODE);
+  if (pipedes_read == -1 || output_descriptor == -1)
+  {
+    perror("Open function call error");
+    exit(EXIT_FAILURE);
+  }
+
+  while ((bytes_read = read(pipedes_read, buffer, WRITE_CHUNK)))
+  {
+    if (bytes_read == -1)
+    {
+      perror("Read function call error");
+      exit(EXIT_FAILURE);
+    }
+    if (write(output_descriptor, buffer, bytes_read) == -1)
+    {
+      perror("Write pipe function call error.");
+      exit(EXIT_FAILURE);
+    }
+    if (bytes_read)
+    {
+      printf("-----\nRecieved data: ");
+      for (int i = 0; i < bytes_read; i++)
+      {
+        printf("%c", buffer[i]);
+      }
+      printf("\n");
+    }
+    if (!bytes_read)
+    {
+      sleep(random_sleep());
+    }
+  }
+  close(output_descriptor);
+  close(pipedes_read);
+  exit(EXIT_SUCCESS);
+}
