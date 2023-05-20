816b34e16d26d1e1fcc4bb38c02b832223a58c0e
Milosz B <miloszbargiel@gmail.com>
2023-05-20 21:25:15 +0200

    Fix a typo in my_mq_getattr function error

--- cw8/mqsource/my_mq_getattr.c
+++ cw8/mqsource/my_mq_getattr.c
@@ -4,7 +4,7 @@ void my_mq_getattr(mqd_t mqdes, struct mq_attr *attr)
 {
     if (mq_getattr(mqdes, attr) == -1)
     {
-        perror("Mq_getaddr function call error");
+        perror("Mq_getattr function call error");
         exit(EXIT_FAILURE);
     }
 }
--- cw8/mqsource/my_mq_getattr.h
+++ cw8/mqsource/my_mq_getattr.h
@@ -4,6 +4,6 @@
 
 #ifndef MY_MQ_GETATTR_H
 #define MY_MQ_GETATTR_H
-//Function to retrieve attributes of mq with error handling. 
+// Function to retrieve attributes of mq with error handling.
 void my_mq_getattr(mqd_t mqdes, struct mq_attr *attr);
 #endif
