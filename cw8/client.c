24c1c6acddcc1c6e4f108b701cde9965160121d0
Milosz B <miloszbargiel@gmail.com>
2023-05-20 21:26:17 +0200

    Add code for cw8 client

---
+++ cw8/client.c
@@ -0,0 +1,128 @@
+#include <mqueue.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <regex.h>
+
+#include "mqsource/my_mq_open.h"
+#include "mqsource/my_mq_unlink.h"
+#include "mqsource/constants.h"
+#include "mqsource/my_mq_send.h"
+#include "mqsource/my_mq_recieve.h"
+
+char mq_name[MQ_NAME_CHAR_LIMIT];
+
+void exit_cleanup();
+void signal_cleanup();
+int validate_input(char *user_input);
+void get_input(char *user_input, ssize_t *input_size, int max_input_len);
+
+int main()
+{
+    struct mq_attr default_mq = {
+        .mq_maxmsg = DEFAULT_MSG_AMT,
+        .mq_msgsize = DEFAULT_MSGSIZE_IN_BYTES,
+        .mq_flags = DEFAULT_MQ_FLAGS,
+        .mq_curmsgs = 0};
+
+    pid_t pid = getpid();
+    int prio;
+
+    sprintf(mq_name, "%s%d", MQ_PREFIX, pid);
+
+    if (atexit(exit_cleanup) == -1)
+    {
+        perror("Atexit function call error");
+        _exit(EXIT_FAILURE);
+    }
+
+    if (signal(SIGINT, signal_cleanup) == SIG_ERR)
+    {
+        perror("Signal function call error");
+        _exit(EXIT_FAILURE);
+    }
+
+    mqd_t client_mq = my_mq_open(mq_name, O_RDONLY | O_CREAT | O_EXCL, OPEN_MODE, &default_mq);
+    mqd_t server_mq = my_mq_open(SERVER_MQ_NAME, O_WRONLY, OPEN_MODE, NULL);
+
+    struct mq_attr mq_attributes;
+    my_mq_getattr(server_mq, &mq_attributes);
+    char msg_buffer[mq_attributes.mq_msgsize];
+    char response[mq_attributes.mq_msgsize];
+
+    while (1)
+    {
+        size_t *input_size = BASE_INPUT_SIZE;
+        char *user_input = (char *)malloc(input_size);
+        int max_msg_size = mq_attributes.mq_msgsize - (strlen(mq_name) + QUERY_DELIM_LEN);
+        get_input(user_input, &input_size, max_msg_size);
+
+        if (*user_input == 'q')
+        {
+            printf("Exiting...\n");
+            exit(EXIT_SUCCESS);
+        }
+
+        sprintf(msg_buffer, "%s%s%s", mq_name, QUERY_DELIM, user_input);
+        my_mq_send(server_mq, msg_buffer, sizeof(msg_buffer), DEFAULT_PRIO);
+        printf("Computing...\n");
+        my_mq_recieve(client_mq, response, sizeof(response), &prio);
+        sleep(COMPUTATION_TIME_IN_S);
+        free(user_input);
+
+        printf("%s\n", response);
+    }
+}
+
+int validate_input(char *user_input)
+{
+    int is_valid_input;
+    regex_t input_regexr;
+    is_valid_input = regcomp(&input_regexr, VALID_INPUT_REGEXR, REG_EXTENDED);
+    is_valid_input = regexec(&input_regexr, user_input, 0, NULL, 0);
+    regfree(&input_regexr);
+    if (is_valid_input == REG_NOMATCH)
+    {
+        fprintf(stderr, "Wrong input!\n");
+        return WRONG_INPUT;
+    }
+    return CORRECT_INPUT;
+}
+
+void get_input(char *user_input, ssize_t *input_size, int max_input_len)
+{
+    int input_len, validation;
+
+    printf("\nProvide an expression in a format <operand> <operator> <operand>\n");
+    printf("Separate each expression element using a single whitespace (regexr requirement)\n");
+    printf("Avaliable operands: +, -, *, /, (Max expression length: %d)\n", max_input_len);
+    printf("Alternatively you can type in \"q\" to exit the client program\n");
+    input_len = getline(&user_input, &input_size, stdin);
+
+    if (input_len > max_input_len)
+    {
+        fprintf(stderr, "Provided message is too long!\n");
+        exit(EXIT_FAILURE);
+    }
+    if (input_len == -1)
+    {
+        perror("Getline function call error");
+        exit(EXIT_FAILURE);
+    }
+    validation = validate_input(user_input);
+    if (validation == WRONG_INPUT)
+    {
+        get_input(user_input, input_size, max_input_len);
+    }
+}
+
+void exit_cleanup()
+{
+    my_mq_unlink(mq_name);
+}
+
+void signal_cleanup()
+{
+    exit(EXIT_SUCCESS);
+}
